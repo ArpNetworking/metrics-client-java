d6e33358fe856d0eb73079a913502bb80eb69844
Ryan Ascheman <rascheman@groupon.com>
2016-09-27 16:23:34 -0700

    add release build script

---
+++ release_buil.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+xmllint --xpath "/*[local-name()='project']/*[local-name()='version'][1]/text()" pom.xml | grep '\-SNAPSHOT'
+retval=$?
+if [ $retval == 0 ]; then
+  ./mvnw -DskipDeploy=false clean deploy -U
+else
+  ./mvnw -DskipDeploy=false -DskipRollerUpload=false clean deploy -U
+fi
